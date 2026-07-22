166315c97224a3db8cfc56d1a350a19e10756bb4
Chibuzor Orah <orahchibuzor17@gmail.com>
2026-07-22 02:48:18 -0700

    complete Day 15 - JavaScript Fundamentals

---
+++ week-02-html-css/script.js
@@ -0,0 +1,23 @@
+console.log("Welcome to JavaScript!");
+let name = "Chibuzor";
+console.log(name);
+
+let lastName = "Orah";
+console.log(lastName);
+
+let age = 24;
+console.log(age);
+
+let isLearning = true;
+console.log(isLearning);
+
+let university = "University of Abuja";
+let goal = "AI Engineer";
+console.log(`I'm studying at ${university} and my goal is to become an ${goal}.`); 
+
+let a = 10;
+let b = 5;
+console.log(a + b);
+console.log(a - b);
+console.log(a * b);
+console.log(a / b);
